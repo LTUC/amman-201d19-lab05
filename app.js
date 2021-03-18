1015790aec9c4972b2a65a41aaef2dbc5a1d6fe3
NadaFraihat <nada997fraihat@gmail.com>
2021-03-18 16:11:13 +0200

    Problem5 is solved

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4 ]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
     let summationResult =sum(sum(sumArr[0],sumArr[1])[0],sumArr[2]); 
@@ -98,11 +98,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+let result =1;
+result= multiply(multiply(multArr[0],multArr[1])[0],multArr[2]);
+return[result[0],'The numbers '+multArr[0]+','+multArr[1]+','+multArr[2]+ ' have a product of '+result[0]+'.'];
 }
-
+//console.log(multiplyArray(testArray));
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 

9524f67f0c1a146ce9269a7db899929c656546c6
saedjabali <saad.jabali@yahoo.com>
2021-03-19 20:08:29 +0200

    4th Problem

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let x = a+b;
 return [x,'The sum of '+a+' and '+b+' is '+x+'.'];
  
 }
- console.log(sum(4,5,8));
+ console.log(sum(4,7));
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -52,10 +52,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line}
-let sum = a + b + c;
-let multiply = a * b* c;
+let z = sum(a,b)[0];
+let s = sum(z,c)[0];
 
-return[sum,multiply,a+' and '+b+' and '+c+' sum to '+sum+'.','The product of '+a+' and '+b+' and '+c+' is '+multiply+'.'];
+let y = multiply(a,b)[0];
+let m = multiply(y,c)[0];
+
+return[s,m,a+' and '+b+' and '+c+' sum to '+s+'.','The product of '+a+' and '+b+' and '+c+' is '+m+'.'];
 
 }
 
@@ -78,12 +81,24 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    let z =sum(sumArr[0],sumArr[1])[0];
+
+    let s =sum(z,sumArr[2])[0];
+
+
+
+
+
+return[s,sumArr+' was passed in as an array of numbers, and '+s+' is their sum.'];
 
 }
+console.log(testArray);
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -127,6 +142,8 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+
+
 
 }
 

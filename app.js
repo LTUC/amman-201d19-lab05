abf07093be8b2eb1c309de73d6db188d6c775751
SUMAYAGHANEM <sumaghanem9@gmail.com>
2021-03-27 08:19:29 -0700

    1

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
-}
+        let result = a + b ; //eslint-disable-line
+    return [result , 'The sum of '+a+ ' and ' +b+' is '+ result + '.'];
+    }
+    // Here is the test for sum(); uncomment it to run it
+    testSum(4, 7);
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
@@ -27,9 +30,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
-}
-
+    // let product2 = a * b;
+    //     return [product2, 'The product of '+a+' and '+9 + ' is '+product2+ '.']
+    // }
+    
+    // Here is the test for multiply(); uncomment it to run it
+    // testMultiply(5,9);
+    
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
@@ -48,9 +55,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
-}
-
+    // let results= a+b+c;
+    // let multiply= a*b*c;
+    // return [ results,multiply,'4 and 7 and 5 sum to 16.',
+    // 'The product of '+a+' and '+b+' and '+c+' is '+multiply+'.' ];
+    
+    // }
+    
+    // Here is the test for sumAndMultiply(); uncomment it to run it
+    // testSumAndMultiply(4,7,5);
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
@@ -58,27 +71,36 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its 
+single argument and then returns an array where the first element is the sum of the 
+numbers in the array, and the second element is a string that EXACTLY follows this
+ example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do
+ addition, use your sum() function that you've already created. You're going to have 
+ to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it
+ is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
-
-function sumArray(sumArr) { //eslint-disable-line
-
-}
+// let resultArray = [];
+// function sumArray(sumArr) { //eslint-disable-line
+//  let result3 = testArray[0]+testArray[1]+testArray[2];
+    
+//     return [ result3,'2,3,4 was passed in as an array of numbers, and 9 is their sum.'];
+    // return [ result3,testArray[0],testArray[1],testArray[2]+' was passed in as an array of numbers, and '+result3+' is their sum.']   
+// }
 
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-
+// testArray[0],testArray[1],testArray[2]+' was passed in as an array of numbers, and '+result3+' is their sum.'
 /////////////////////////////////////
 /* Problem 5
 Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
@@ -125,4 +147,6 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// let rarray=[1,2,3];
+// let result = rarray[0]+rarray[1]+rarray[2];
+// console.log(result);

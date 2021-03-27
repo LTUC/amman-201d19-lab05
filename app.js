e79dc90013fc7594b582ee8858fbbc76d29bc223
SUMAYAGHANEM <sumaghanem9@gmail.com>
2021-03-27 08:26:37 -0700

    third

--- app.js
+++ app.js
@@ -54,16 +54,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function sumAndMultiply(a, b, c) { //eslint-disable-line
-    // let results= a+b+c;
-    // let multiply= a*b*c;
-    // return [ results,multiply,'4 and 7 and 5 sum to 16.',
-    // 'The product of '+a+' and '+b+' and '+c+' is '+multiply+'.' ];
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let results= a+b+c;
+    let multiply= a*b*c;
+    return [ results,multiply,'4 and 7 and 5 sum to 16.',
+    'The product of '+a+' and '+b+' and '+c+' is '+multiply+'.' ];
     
-    // }
+    }
     
     // Here is the test for sumAndMultiply(); uncomment it to run it
-    // testSumAndMultiply(4,7,5);
+    testSumAndMultiply(4,7,5);
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
